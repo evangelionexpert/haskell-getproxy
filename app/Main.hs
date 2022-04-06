212f31df7992ec8c1fd5792f9a4e72c266114410
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2022-04-06 13:44:31 +0700

    MAIN finally WORKS!!!!!!!! PREVIOUSLY IT DIDNT!!!!

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,11 @@ module Main where
 import GetProxy
 
 import System.Environment (getArgs)
-import Text.Read (readMaybe)
 import Network.Socket (PortNumber)
+import Text.Read (readMaybe)
+import Control.Monad (join)
+import Control.Applicative (liftA)
+import Data.Maybe (listToMaybe)
 
 main :: IO ()
 main = do 
@@ -12,8 +15,8 @@ main = do
     let succMsg = "Starting proxy..."
     let exitMsg = "Exiting..."
 
-    arg <- head <$> getArgs
+    args <- getArgs
 
-    case readMaybe arg :: Maybe PortNumber of 
+    case join $ liftA readMaybe $ listToMaybe args :: Maybe PortNumber of 
         Just port -> putStrLn succMsg >> startProxy port >> putStrLn exitMsg
         Nothing -> putStrLn helpMsg
